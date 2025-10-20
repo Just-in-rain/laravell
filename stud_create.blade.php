1873e10bf975c371348cb03e4b0673c080ea219e
Just-in-rain <0323-2652@lspu.edu.ph>
2025-10-20 18:22:36 +0800

    Create stud_create.blade.php

---
+++ stud_create.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Add Student</title>
+</head>
+<body>
+    <form action="/create" method="post">
+        @csrf
+        <table>
+            <tr>
+                <td>Name:</td>
+                <td><input type="text" name="stud_name"></td>
+            </tr>
+            <tr>
+                <td colspan="2"><input type="submit" value="Add student"></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
