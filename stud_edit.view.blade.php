0cdd37564e956d48e99366e6e7fe13cda9fae9d3
Just-in-rain <0323-2652@lspu.edu.ph>
2025-10-20 18:23:28 +0800

    Create student edit view template

---
+++ stud_edit.view.blade.php
@@ -0,0 +1,15 @@
+<html>
+<head><title>Edit Students</title></head>
+<body>
+<table border="1">
+<tr><td>ID</td><td>Name</td><td>Edit</td></tr>
+@foreach ($users as $user)
+<tr>
+<td>{{ $user->id }}</td>
+<td>{{ $user->name }}</td>
+<td><a href="edit/{{ $user->id }}">Edit</a></td>
+</tr>
+@endforeach
+</table>
+</body>
+</html>
